3ce9229b03e8627971e51108169d7f34c75d32fe
Nikola Kelava <nkelava.dev@gmail.com>
2026-03-29 23:39:36 +0200

    test: write tests for button component

--- examples/element-factory/src/button.test.js
+++ examples/element-factory/src/button.test.js
@@ -1,9 +1,34 @@
+import { screen } from '@testing-library/dom';
 import { createButton } from './button.js';
+import userEvent from '@testing-library/user-event';
 
-describe.todo('createButton', () => {
-  it('should create a button element', () => {});
+describe('createButton', () => {
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
 
-  it('should have the text "Click Me"', () => {});
+  it('should create a button element', () => {
+    const button = createButton();
 
-  it('should change the text to "Clicked!" when clicked', async () => {});
+    expect(button).toBeInstanceOf(HTMLButtonElement);
+    expect(button.tagName).toBe('BUTTON');
+  });
+
+  it('should have the text "Click Me"', () => {
+    document.body.appendChild(createButton());
+
+    const button = screen.getByRole('button', { name: 'Click Me' });
+
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should change the text to "Clicked!" when clicked', async () => {
+    document.body.appendChild(createButton());
+
+    const button = screen.getByRole('button', { name: 'Click Me' });
+
+    await userEvent.click(button);
+
+    expect(button.textContent).toBe('Clicked!');
+  });
 });
