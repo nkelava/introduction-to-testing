7c8af5868bdc55258ad0814c55ab6d1c7a912f1b
Nikola Kelava <nkelava.dev@gmail.com>
2026-03-31 12:08:31 +0200

    test: add tests for alert button components

--- examples/element-factory/src/alert-button.test.jsx
+++ examples/element-factory/src/alert-button.test.jsx
@@ -1,14 +1,38 @@
+import { vi } from 'vitest';
 import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { AlertButton } from './alert-button';
 
-describe.todo('AlertButton', () => {
+describe('AlertButton', () => {
   beforeEach(() => {});
 
   afterEach(() => {});
 
-  it('should render an alert button', async () => {});
+  it('should render an alert button', async () => {
+    render(<AlertButton />);
+    const button = screen.getByRole('button', { name: /trigger alert/i });
 
-  it('should trigger an alert', async () => {});
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should trigger an alert', async () => {
+    const onSubmit = vi.fn(() => {});
+
+    render(
+      <AlertButton onSubmit={onSubmit} defaultMessage={'Default Message'} />,
+    );
+
+    const input = screen.getByLabelText('Message');
+    const button = screen.getByRole('button', { name: /trigger alert/i });
+
+    await act(async () => {
+      await userEvent.clear(input);
+      await userEvent.type(input, 'Hello');
+      await userEvent.click(button);
+    });
+
+    expect(onSubmit).toHaveBeenCalled();
+    expect(onSubmit).toHaveBeenCalledWith('Hello');
+  });
 });
