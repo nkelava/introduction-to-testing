25663ff60b8fa2be6cc5955793b2df9009e39e20
Nikola Kelava <nkelava.dev@gmail.com>
2026-03-31 14:07:13 +0200

    test: add tests for log component

--- examples/logjam/src/log.test.js
+++ examples/logjam/src/log.test.js
@@ -1,4 +1,28 @@
 import { expect, it, vi, beforeEach, afterEach, describe } from 'vitest';
 import { log } from './log';
 
-describe.todo('logger', () => {});
+describe('logger', () => {
+  describe('development', () => {
+    it('logs to the console in development mode', () => {
+      const logSpy = vi.spyOn(console, 'log');
+
+      log('Development log.', {
+        mode: 'development',
+      });
+
+      expect(logSpy).toHaveBeenCalledWith('Development log.');
+    });
+  });
+
+  describe('production', () => {
+    it('should not call console in production mode', () => {
+      const logSpy = vi.spyOn(console, 'log');
+
+      log('Production log.', {
+        mode: 'production',
+      });
+
+      expect(logSpy).not.toHaveBeenCalled();
+    });
+  });
+});
