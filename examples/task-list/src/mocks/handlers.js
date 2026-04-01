80b122a8fd6684d902a44bcc35ee0524535fd948
Nikola Kelava <nkelava.dev@gmail.com>
2026-04-01 16:13:51 +0200

    mock: add get and post handers for tasks

--- examples/task-list/src/mocks/handlers.js
+++ examples/task-list/src/mocks/handlers.js
@@ -7,8 +7,18 @@ const createTask = (title) => ({
   id: `${id++}`,
   title,
   completed: false,
-  createdAt: new Date('02-29-2024').toISOString(),
-  lastModified: new Date('02-29-2024').toISOString(),
+  createdAt: new Date('04-01-2026').toISOString(),
+  lastModified: new Date('04-01-2026').toISOString(),
 });
 
-export const handlers = [];
+export const handlers = [
+  http.get('/api/tesks', () => {
+    return HttpResponse.json(tasks);
+  }),
+  http.post('/api/tasks', async ({ request }) => {
+    const { title } = await request.json();
+    const task = createTask(title);
+
+    return HttpResponse.json(task);
+  }),
+];
